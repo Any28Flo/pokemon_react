55c3daaa618a228e6f21bf89ff036a053c968444
Analin Flores <foglzerika@gmail.com>
2022-05-23 13:05:03 -0500

    feat | Creating Spinner component

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Spinner = () => {
+    return (
+        <div className="wrapper">
+            <div className="pokeball">
+            </div>
+        </div>
+    );
+};
+
+export default Spinner;
